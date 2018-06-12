dd303c8b4115a9d58ab729f5801f0d7cc71cefb8
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-06-12 17:59:33 -0500

    created map and restaurants-list components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './components/map/map.component';
+import { RestaurantsListComponent } from './restaurants-list/restaurants-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    RestaurantsListComponent
   ],
   imports: [
     BrowserModule
