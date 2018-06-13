fcf3fc4706fa2c05b8214db7ba12ed94c5fa603e
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-06-13 15:46:50 -0500

    drag radius

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -11,24 +11,41 @@ export class MapComponent implements OnInit {
   lat = 19.438655;
   lng = -99.1305917;
   locationChosen = false;
+  zoom = 16;
   restaurants;
+  radius = 300;
+  restaurantsInRadius = [];
+  m;
 
   constructor(private _melpService: MelpService) { }
 
   ngOnInit() {
     this._melpService.getRestaurants()
-    .subscribe((data) => {
-      this.restaurants = data.restaurants;
-      console.log(data);
-    });
-
+      .subscribe((data) => {
+        this.restaurants = data.restaurants;
+        console.log(data);
+      });
   }
 
   onChoseLocation(event) {
     this.lat = event.coords.lat;
     this.lng = event.coords.lng;
     this.locationChosen = true;
-    console.log(event);
+  }
+
+  restaurantsOnRadius() {
+    this.restaurants.map(restaurant => {
+      if (this.getDistance(restaurant.address.location.lat, restaurant.address.location.lng)
+        <= this.radius
+        && this.restaurantsInRadius.indexOf(restaurant) < 0) {
+        this.restaurantsInRadius.push(restaurant);
+      }
+    });
+  }
+
+  mapDrag(event) {
+    this.onChoseLocation(event);
+    this.restaurantsOnRadius();
   }
 
 }
